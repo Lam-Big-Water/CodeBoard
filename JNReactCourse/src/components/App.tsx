f0f799605b297babbf7be485bfb81473621c6f74
Sam Lam <xlin67948gmail.com>
2024-10-10 12:03:57 +0800

    the reducer confuse is resolve

--- JNReactCourse/src/components/App.tsx
+++ JNReactCourse/src/components/App.tsx
@@ -3,66 +3,100 @@ import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
+import Loader from "./Loader";
+import Fail from "./Fail";
+import StartScreen from "./StartScreen";
+
+import Question from "./Question";
+
 type QuestionTypes = {
-    question: string;
-    options: any[];
-    correctOption: number;
-    points: number;
-    // status: "loading" | "error" | "ready" | "active" | "finished";
-}
+  question: string;
+  options: any[];
+  correctOption: number;
+  points: number;
+};
+
+// type StatusType = {
+//     status: "loading" | "error" | "ready" | "active" | "finished";
+// }
 
 type State = {
-    questions: QuestionTypes[];
-}
+  questions: QuestionTypes[];
+  status: string;
+};
 
 const initialState = {
-    questions: [],
-    // status: "loading",
+  questions: [],
+  status: "loading",
 };
 
 interface DataReceived {
-    type: "DATA_RECEIVED";
-    payload: QuestionTypes[];
+  type: "DATA_RECEIVED";
+  payload: QuestionTypes[];
 }
 
-type ActionType = DataReceived;
+interface DATAFailed {
+  type: "DATA_FAILED";
+}
 
+interface QuestionStart {
+  type: "START";
+}
+
+type ActionType = DataReceived | DATAFailed | QuestionStart;
 
 function reducer(state: State, action: ActionType) {
-    switch(action.type) {
-        case "DATA_RECEIVED":
-            return {
-                ...state,
-                questions: action.payload,
-                // status: "ready"
-            };
-        default:
-            throw new Error("Action unknown");
-    }
+  switch (action.type) {
+    case "DATA_RECEIVED":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "DATA_FAILED":
+      return {
+        ...state,
+        status: "error",
+      };
+    case "START":
+      return {
+        ...state,
+        status: "active",
+      };
+    default:
+      throw new Error("Action unknown");
+  }
 }
 
 const App = () => {
-    const [state, dispatch] = useReducer(reducer, initialState)
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
 
-    useEffect(() => {
-        fetch("http://localhost:8000/questions")
-            .then((res) => res.json())
-            .then((data) => {
-                dispatch({type: "DATA_RECEIVED", payload: data})
-            })
-            .catch((err) => console.error("Error"));
-    }, []);
-    return (
-        <div className="app">
-            <Header />
+  const numQuestions = questions.length;
 
-            <Main>
-                <>
-                    
-                </>
-            </Main>
-        </div>
-    )
-}
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => {
+        dispatch({ type: "DATA_RECEIVED", payload: data });
+      })
+      .catch((err) => dispatch({ type: "DATA_FAILED" }));
+  }, []);
+  return (
+    <div className="app">
+      <Header />
+
+      <Main>
+        <>
+          {status === "loading" && <Loader />}
+          {status === "error" && <Fail />}
+          {status === "ready" && (
+            <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+          )}
+          {status === "active" && <Question />}
+        </>
+      </Main>
+    </div>
+  );
+};
 
 export default App;
---
+++ JNReactCourse/src/components/Fail.tsx
@@ -0,0 +1,10 @@
+
+const Fail = () => {
+  return (
+    <p className="error">
+      <span></span> There was an error fetching questions.
+    </p>
+  )
+}
+
+export default Fail
