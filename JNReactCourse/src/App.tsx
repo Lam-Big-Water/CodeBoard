cf2ff7060a3e5833542e7da4f7de4cb8c1657df5
Sam Lam <xlin67948gmail.com>
2024-10-12 11:51:00 +0800

    the pricing & product pages is done

--- JNReactCourse/src/App.tsx
+++ JNReactCourse/src/App.tsx
@@ -1,11 +1,16 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Homepage from "./pages/Homepage";
+import Pricing from "./pages/Pricing";
+import Product from "./pages/Product";
+
 function App () {
     return (
         <BrowserRouter>
             <Routes>
                 <Route index element={<Homepage />}/>
+                <Route path="pricing" element={<Pricing />}/>
+                <Route path="product" element={<Product />}/>
             </Routes>
         </BrowserRouter>
     )
