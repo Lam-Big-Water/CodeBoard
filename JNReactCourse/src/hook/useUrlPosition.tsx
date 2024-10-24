fecfbcccd14510e74c9b03c09441350318c729df
Sam Lam <xlin67948gmail.com>
2024-10-24 23:05:22 +0800

    delete

--- JNReactCourse/src/hook/useUrlPosition.tsx
+++
@@ -1,9 +0,0 @@
-import { useSearchParams } from "react-router-dom";
-
-export function useUrlPosition() {
-  const [searchParams] = useSearchParams();
-  const lat = Number(searchParams.get("lat"));
-  const lng = Number(searchParams.get("lng"));
-
-  return [lat, lng];
-}
