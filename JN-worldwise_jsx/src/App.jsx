ad097473e58ba8768778294faa03438a21c5f190
Sam Lam <xlin67948gmail.com>
2024-12-12 10:24:39 +0800

    worldwise is done

---
+++ JN-worldwise_jsx/src/App.jsx
@@ -0,0 +1,54 @@
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+import HomePage from "./pages/Homepage";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+
+import ProtectedRoute from "./pages/ProtectedRoute";
+import AppLayout from './pages/AppLayout'
+
+import { CitiesProvider } from "./contexts/CitiesContext";
+import { AuthProvider } from "./contexts/FakeAuthContext";
+import CityList from "./components/CityList";
+import City from "./components/City";
+import CountryList from "./components/CountryList";
+import Form from "./components/Form";
+
+const App = () => {
+  return (
+    <AuthProvider>
+      <CitiesProvider>
+        <BrowserRouter>
+
+        <Routes>
+          <Route index element={<HomePage />} />
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="login" element={<Login />} />
+
+          <Route path="app" 
+          element={
+          <ProtectedRoute>
+            <AppLayout />
+          </ProtectedRoute>
+          }
+          >
+            <Route index element={<Navigate replace to="cities" />} />
+            <Route path="cities" element={<CityList />} />
+            <Route path="cities/:id" element={<City />} />
+            <Route path="countries" element={<CountryList />} />
+            <Route path="form" element={<Form />} />
+          </Route>
+
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+
+        </BrowserRouter>
+      </CitiesProvider>
+    </AuthProvider>
+      
+  )
+}
+
+export default App
